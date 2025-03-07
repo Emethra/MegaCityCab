3faa368431e21a00662e2bed026b49fe734fd567
Hasara123 <hasarahithaishi@gmail.com>
2025-03-07 20:34:01 +0530

    Created DeleteBookingServlet

---
+++ src/java/com/servlet/DeleteBookingServlet.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.servlet;
+
+/**
+ *
+ * @author Hasara Hithaishi
+ */
+
+import com.conn.DbConnect;
+import com.dao.BookingDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/delete")
+public class DeleteBookingServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int cid = Integer.parseInt(req.getParameter("cid"));
+        
+        BookingDAO dao = new BookingDAO(DbConnect.getConn());
+        
+        boolean f = dao.deleteBookingById(cid);
+        HttpSession session = req.getSession();
+        if (f) {
+            session.setAttribute("succMsg", "Booking Deleted Successfully..");
+            resp.sendRedirect("ManageBooking.jsp");
+        } else {
+            session.setAttribute("failedMsg", "Something wrong on server..");
+            resp.sendRedirect("ManageBooking.jsp");
+        }
+    }
+}
