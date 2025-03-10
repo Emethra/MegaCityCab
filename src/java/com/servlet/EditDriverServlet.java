2d1e6d8ef5019575bc040f7495d6e92496d085c8
Hasara123 <hasarahithaishi@gmail.com>
2025-03-10 21:28:10 +0530

    Adding DeleteDriverServlet

--- src/java/com/servlet/EditDriverServlet.java
+++ src/java/com/servlet/EditDriverServlet.java
@@ -20,29 +20,31 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet("/AddDriverServlet")
-public class AddDriveServlet extends HttpServlet {
+@WebServlet("/EditDriverServlet")
+public class EditDriverServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         try {
+            int id = Integer.parseInt(request.getParameter("id"));
             String name = request.getParameter("name");
             String carModel = request.getParameter("carModel");
             String vehicleNumber = request.getParameter("vehicleNumber");
             String phoneNumber = request.getParameter("phoneNumber");
 
             Driver driver = new Driver(name, carModel, vehicleNumber, phoneNumber);
-            DriverDAO dao = new DriverDAO(DbConnect.getConn());
+            driver.setId(id);
             
-            if (dao.addDriver(driver)) {
-                session.setAttribute("succMsg", "Driver Added Successfully");
+            DriverDAO dao = new DriverDAO(DbConnect.getConn());
+            if (dao.updateDriver(driver)) {
+                session.setAttribute("succMsg", "Driver Updated Successfully");
             } else {
-                session.setAttribute("failedMsg", "Failed to Add Driver");
+                session.setAttribute("failedMsg", "Failed to Update Driver");
             }
         } catch (Exception e) {
             e.printStackTrace();
-            session.setAttribute("failedMsg", "Error Adding Driver: " + e.getMessage());
-         }
+            session.setAttribute("failedMsg", "Error Updating Driver: " + e.getMessage());
+        }
         response.sendRedirect("adminManageDrivers.jsp");
     }
 }
